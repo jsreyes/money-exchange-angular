3a8489588aebb3f32c52391517dad44ab4e9dbb1
Javier Reyes <jreyes@portalfinance.co>
2019-05-06 23:16:57 -0500

    Complete test cases to app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,51 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { By } from '@angular/platform-browser';
+
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, HeaderComponent, BodyComponent, FooterComponent
       ],
-    }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+  });
+
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should have as title 'app'`, () => {
+    expect(component.title).toEqual('app');
+  });
+
+  it('should have a header component', () => {
+    const de = fixture.debugElement.query(By.directive(HeaderComponent));
+    expect(de).not.toBeNull();
+  });
+
+  it('should have a body component', () => {
+    const de = fixture.debugElement.query(By.directive(BodyComponent));
+    expect(de).not.toBeNull();
+  });
+
+  it('should have a footer component', () => {
+    const de = fixture.debugElement.query(By.directive(FooterComponent));
+    expect(de).not.toBeNull();
+  });
+
+  it('should have a class container in main div', () => {
+    const de = fixture.debugElement.query(By.css('.container'));
+    expect(de).toBeTruthy();
+  });
 });
